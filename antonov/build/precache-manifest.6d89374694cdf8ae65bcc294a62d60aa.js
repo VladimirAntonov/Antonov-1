6e8adcbec7560c0864727eede15c727118c81b32
vladimirantonov <vova.antonov.99@bk.ru>
2019-12-16 22:06:01 +0300

    1

---
+++ antonov/build/precache-manifest.6d89374694cdf8ae65bcc294a62d60aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beaf828fd608847443b01e4d26992b24",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c460108481b05d8db622",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "644b9ba17837766c6e6f",
+    "url": "/static/js/2.2f76ddc8.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/static/js/2.2f76ddc8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c460108481b05d8db622",
+    "url": "/static/js/main.a8d0135a.chunk.js"
+  },
+  {
+    "revision": "57e80b443c0a26f7d738",
+    "url": "/static/js/runtime-main.a5b535b5.js"
+  }
+]);
